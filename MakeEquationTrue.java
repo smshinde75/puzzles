dcecc271a14235adb6a3a8566de319da6bb30b14
smshinde75 <smshinde@gmail.com>
2023-01-08 19:21:09 +0530

    Add files via upload

---
+++ MakeEquationTrue.java
@@ -0,0 +1,32 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MakeEquationTrue {
+    public static void main(String[] args) {
+        ScriptEngineManager sem = new ScriptEngineManager();
+        ScriptEngine engine = sem.getEngineByName("JavaScript");
+        ArrayList<String> operations = new ArrayList<>(Arrays.asList("+", "-", "*", "/"));
+        operations.forEach(operation1 -> {
+            operations.forEach(operation2 -> {
+                operations.forEach(operation3 -> {
+                    operations.forEach(operation4 -> {
+                        try {
+                            Object object = engine.eval("18" + operation1 + "12" + operation2 + "4" + operation3 + "5");
+                            Integer evaluatedInteger = Integer.parseInt(object + "");
+                            if (evaluatedInteger == 59) {
+                                System.out.println("18" + operation1 + "12" + operation2 + "4" + operation3 + "5" + "=59");
+                            }
+                        } catch (ScriptException e) {
+                            throw new RuntimeException(e);
+                        } catch (NumberFormatException nfe) {
+                            //skipping non integers
+                        }
+                    });
+                });
+            });
+        });
+    }
+}
